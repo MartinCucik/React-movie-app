bf23d6b158265f8ff1c7d49de77de3f6157ed498
Martin Cucik <mattko432@azet.sk>
2022-12-15 20:33:48 +0100

    Updates

---
+++ precache-manifest.2b39a02326c1fa5844843edc851f66fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a4968c32c3c8cfa8ffe",
+    "url": "/React-movie-app/static/css/main.690ef26e.chunk.css"
+  },
+  {
+    "revision": "8a4968c32c3c8cfa8ffe",
+    "url": "/React-movie-app/static/js/main.8a4968c3.chunk.js"
+  },
+  {
+    "revision": "3da4e6a8535dd6d8027b",
+    "url": "/React-movie-app/static/js/1.3da4e6a8.chunk.js"
+  },
+  {
+    "revision": "04f88590cde050ae79bd",
+    "url": "/React-movie-app/static/js/2.04f88590.chunk.js"
+  },
+  {
+    "revision": "bad59300f3cf9a84e118",
+    "url": "/React-movie-app/static/js/runtime~main.bad59300.js"
+  },
+  {
+    "revision": "d05a4d21b912489ef8269eca53ca8e8d",
+    "url": "/React-movie-app/static/media/logo2.d05a4d21.jpg"
+  },
+  {
+    "revision": "00f2b289186e4c920f93a2870c0351f5",
+    "url": "/React-movie-app/index.html"
+  }
+];
